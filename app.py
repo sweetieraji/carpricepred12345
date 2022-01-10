f61d87176522326b4090c8f2d458d0f38947b90c
sweetieraji <97399044+sweetieraji@users.noreply.github.com>
2022-01-10 16:12:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from flask  import Flask, render_template, request
-import jsonify
-import requests
+
 import pickle
 import numpy as np
-import sklearn
-from sklearn.preprocessing import StandardScaler
+
+
 app = Flask(__name__)
+
 model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
+
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
 
 
-standard_to = StandardScaler()
+
 @app.route("/predict", methods=['POST'])
 def predict():
     Fuel_Type_Diesel=0
